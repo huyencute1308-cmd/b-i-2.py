600e307c316c78985bf50bd830bc37296bada6fd
huyencute1308-cmd <huyencute1308@gmail.com>
2025-11-26 20:55:29 +0700

    Fix variable name and formatting in 1.4.6.py

--- 1.4.6.py
+++ 1.4.6.py
@@ -1,7 +1,6 @@
 number = []
-
-for i in range(2000, 3201):  # bao gồm cả 3200
+for i in range(2000, 3201):  
     if (i % 7 == 0) and ( i % 5 != 0):
         number.append(str(i))
-
 print(",".join(numbers))
+
